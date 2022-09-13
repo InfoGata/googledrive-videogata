43777f1595c245383ad486d5b358cdc1e8f1c43e
Elijah Green <elijahgreen@gmail.com>
2022-09-12 20:15:48 -0700

    Change client Id

--- src/shared.ts
+++ src/shared.ts
@@ -1,5 +1,5 @@
 export const TOKEN_SERVER =
   "https://cloudflare-worker-token-service.audio-pwa.workers.dev/token";
 export const CLIENT_ID =
-  "881268311708-llefv1en769dk3n8c634jspp3jqb6bv7.apps.googleusercontent.com";
+  "842170761773-ltlj9t8cap9q1ad7s1h6tb5084ouelod.apps.googleusercontent.com";
 export const TOKEN_URL = "https://oauth2.googleapis.com/token";
